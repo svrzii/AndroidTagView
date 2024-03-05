6d7d4d43410503c2658791a4574abf7ab0ca8117
Matej Svržnjak <svrzii@users.noreply.github.com>
2024-03-05 14:05:58 +0100

    Update TagView.java

--- androidtagview/src/main/java/co/lujun/androidtagview/TagView.java
+++ androidtagview/src/main/java/co/lujun/androidtagview/TagView.java
@@ -357,7 +357,7 @@ public class TagView extends View {
     private void drawImage(Canvas canvas){
         if (isEnableImage()) {
             // Load your image as a Bitmap
-            Bitmap scaledImageBitmap = Bitmap.createScaledBitmap(mBitmapImage, Math.round(getHeight() - mBorderWidth), Math.round(getHeight() - mBorderWidth), false);
+            Bitmap scaledImageBitmap = Bitmap.createScaledBitmap(mBitmapImage, Math.round(getHeight() - mVerticalPadding), Math.round(getHeight() - mHorizontalPadding), false);
 
             Paint paint = new Paint();
             paint.setAntiAlias(true);
